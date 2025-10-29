41274fc75f86947d698c2a5649d2e453014e3ca9
Jsanchez312 <140443285+Jsanchez312@users.noreply.github.com>
2025-10-28 23:23:51 -0500

    Update 07.c

--- 07.c
+++ 07.c
@@ -1,3 +1,9 @@
+/*************************
+ * Autor: Juan Pablo Sanchez
+ * Fecha:28/10/2025
+ * Profesor: Jhon Corredor
+ * Clase: Sistemas Operativos
+ * **************************/
 #include <stdio.h>
 #include <pthread.h>
 
@@ -27,4 +33,5 @@ int main(void) {
 
     printf("Sum = %lld\n", sum);
     return 0;
+
 }
