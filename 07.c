4ac372d31a5e1413b954cdf9af6a81800280a3b5
Jsanchez312 <140443285+Jsanchez312@users.noreply.github.com>
2025-10-28 23:32:31 -0500

    Update 07.c

--- 07.c
+++ 07.c
@@ -1,9 +1,16 @@
-/*************************
- * Autor: Juan Pablo Sanchez
- * Fecha:28/10/2025
- * Profesor: Jhon Corredor
- * Clase: Sistemas Operativos
- * **************************/
+/*********************************************************************************************
+ * Pontificia Universidad Javeriana
+ * Autor: Juan Sánchez
+ * Materia Sistemas Operativos
+ * Docente: J. Corredor, PhD
+ * Fecha: 27/10/2025
+ * Tema: Posix para la creación de hilos concurrentes:
+ *
+ * Descripción:
+ * "Este código compara ejecución secuencial vs concurrente mostrando diferencias
+ *  en el resultado final de operaciones compartidas"
+ *
+ *********************************************************************************************/
 #include <stdio.h>
 #include <pthread.h>
 
@@ -35,3 +42,4 @@ int main(void) {
     return 0;
 
 }
+
